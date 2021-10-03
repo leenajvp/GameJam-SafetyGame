fb4d461c466b05c6526121632ad61773430859ba
heggasmax <61008091+heggasmax@users.noreply.github.com>
2021-10-03 16:57:53 +0100

    move script added

---
+++ SafetyGame/Assets/Max's scripts/move.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    [SerializeField]
+    float topspeed = 5.0f;
+    [SerializeField]
+    float acceleration = 2.0f;
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + transform.forward * 5 * Time.deltaTime;
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 1.001f);
+        if (colliders.Length == 1)
+        {
+            speed += acceleration * Time.deltaTime;
+            if (speed > topspeed)
+            {
+                speed = topspeed;
+            }
+            transform.position = transform.position + -transform.up * speed * Time.deltaTime;
+        }
+        else
+        {
+            foreach (var col in colliders)
+            {
+                if (col.GetComponent<MeshFilter>().mesh.name == "Plane Instance")
+                {
+                    if (transform.rotation.eulerAngles.y != col.transform.rotation.eulerAngles.y)
+                    {
+                        transform.Rotate(0, (col.transform.rotation.eulerAngles.y) - transform.rotation.eulerAngles.y, 0);
+                    }
+                    if (transform.rotation.eulerAngles.x != col.transform.rotation.eulerAngles.x)
+                    {
+                        transform.Rotate((col.transform.rotation.eulerAngles.x - transform.rotation.eulerAngles.x), 0, 0);
+                    }
+                    if (transform.rotation.eulerAngles.y == col.transform.rotation.eulerAngles.y)
+                    {
+                        if (transform.rotation.eulerAngles.y == 90)
+                        {
+                            float km = col.transform.position.z - transform.position.z;
+                            if (km != 0)
+                                transform.position = transform.position + transform.right * km * Time.deltaTime;
+                        }
+                        else
+                        {
+                            if (transform.rotation.eulerAngles.y == 270)
+                            {
+                                float km = col.transform.position.z - transform.position.z;
+                                if (km != 0)
+                                    transform.position = transform.position + transform.right * km * Time.deltaTime;
+                            }
+                            else
+                            {
+                                float km = col.transform.position.x - transform.position.x;
+                                if (km != 0)
+                                    transform.position = transform.position + transform.right * km * Time.deltaTime;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
