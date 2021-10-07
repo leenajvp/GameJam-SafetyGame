23f094a428f4e945d9cd5fb1a7940e2b1550a4b5
Leena Jarvenpaa <leena.jarvenpaa@windowslive.com>
2021-10-08 00:38:23 +0100

    helmet on off added

--- SafetyGame/Assets/Leena's Scripts/InventoryScripts/HelmetSpot.cs
+++ SafetyGame/Assets/Leena's Scripts/InventoryScripts/HelmetSpot.cs
@@ -3,16 +3,23 @@
 public class HelmetSpot : MonoBehaviour, IPlayerDropSpot
 {
     private SkinnedMeshRenderer mesh;
+    private PlayerCollection player;
     public bool isPlaced { get; set; }
 
     private void Start()
     {
+        player = transform.parent.GetComponent<PlayerCollection>();
         mesh = GetComponent<SkinnedMeshRenderer>();
         isPlaced = false;
     }
 
     private void Update()
     {
+        if (player.helmetCollected == false)
+        {
+            isPlaced = false;
+        }
+
         if (isPlaced == true)
         {
             mesh.enabled = true;
