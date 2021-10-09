33d4a2fa31bde2d35771849702c78b5915d80d2d
Leena Jarvenpaa <leena.jarvenpaa@windowslive.com>
2021-10-09 19:14:36 +0100

    MainMenu done

---
+++ SafetyGame/Assets/Leena's Scripts/Menus/MenuScript.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    [SerializeField] private GameObject PauseMenu = null;
+    [SerializeField] private GameObject CreditsMenu = null;
+    private bool paused = false;
+
+    private void Start()
+    {
+        PauseMenu.SetActive(false);
+        CreditsMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                PauseMenu.SetActive(false);
+            }
+
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ExitPause()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void OpenCreditsMenu()
+    {
+        CreditsMenu.SetActive(true);
+    }
+
+    public void ExitCredits()
+    {
+        CreditsMenu.SetActive(false);
+    }
+
+    public void BackToGame()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
